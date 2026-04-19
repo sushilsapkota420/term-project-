633a0ba28b922a5b998378e701d13efbbffa8f60
BigyanShrestha1 <bshrestha@sfsu.edu>
2026-04-19 05:23:47 +0000

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()); 
 
 let mysteryBoxes = [
   {
